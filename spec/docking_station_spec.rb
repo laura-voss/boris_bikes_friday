ad91bc8902050081106e56ce1067ea62640dd764
Laura Voss <lauravoss@posteo.de>
2022-04-21 17:49:08 +0100

    Finished docking bikes, using instance variables

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,16 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'dock bike at docking station' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'is there a bike at the docking station' do
+    bike = Bike.new
+    subject.dock(bike) 
+    expect(subject.bike).to eq bike
+  end
+
 end
