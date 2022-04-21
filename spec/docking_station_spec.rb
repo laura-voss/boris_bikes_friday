355352f93eb7beaed0783df42d6733e6a9573764
Russell <russellmorbey@hotmail.co.uk>
2022-04-21 15:52:15 +0100

    Finished Making Docking Stations Release Bikes.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,9 @@ require 'docking_station'
 
 describe DockingStation do
   it { expect(subject).to respond_to :release_bike }
+
+  it 'releases working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
